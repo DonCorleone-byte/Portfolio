3eafbdac4c3952bf97107f491d687a5a6018acff
Maxim <maks_umov1996@mail.ru>
2022-12-05 23:09:03 +0600

    add send mail

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,16 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('form').trigger('reset');
+    });
+    return false;
+});
